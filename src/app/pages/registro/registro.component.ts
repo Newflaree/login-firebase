182f78550f61f0a8de0d34583a6cde9d899d4052
Newflare <camiloj.lopez79@gmail.com>
2020-11-02 16:10:02 -0300

    Se agregan todos los archivos

---
+++ src/app/pages/registro/registro.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { UsuarioModel } from '../../models/usuario.model';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+  usuario: UsuarioModel;
+  recordarme = false;
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() { 
+    this.usuario = new UsuarioModel();
+ 
+  }
+
+  onSubmit( form: NgForm ) {
+    if ( form.invalid ) { return; }
+
+    this.auth.nuevoUsuario( this.usuario )
+        .subscribe( resp => {
+          if ( this.recordarme ) {
+            localStorage.setItem('email', this.usuario.email);
+          }
+
+          this.router.navigateByUrl('/home');
+
+        }, (err) => {
+          console.log(err.error.error.message);
+        });
+  }
+}
